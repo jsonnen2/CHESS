37a66eea04b89ff0beff016cfdffae90697b3754
Joshua Sonnen <153052192+jsonnen2@users.noreply.github.com>
2024-03-25 17:31:38 -0700

    Create chess.h

---
+++ chess.h
@@ -0,0 +1,17 @@
+// chess.h
+#ifndef CHESS_H
+#define CHESS_H
+
+// I think this is allowed.
+// #include <iostream>
+
+namespace CHESS
+{
+    class fen_string
+    {
+        public:
+            void print_board();
+    };
+}
+
+#endif
